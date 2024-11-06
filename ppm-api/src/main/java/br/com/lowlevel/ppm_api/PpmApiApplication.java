b619b430b3298e3418b972e01eea2eb71b0caab8
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2024-11-06 08:52:02 -0300

    Initial API

---
+++ ppm-api/src/main/java/br/com/lowlevel/ppm_api/PpmApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lowlevel.ppm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmApiApplication.class, args);
+	}
+
+}
---
+++ ppm-api/src/test/java/br/com/lowlevel/ppm_api/PpmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lowlevel.ppm_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
